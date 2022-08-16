b3d18d393964f9738ef161b310517fd3831839ae
annashcherenkova <a.shcherenkova@gmail.com>
2022-08-16 20:28:01 +0300

    feat: Add guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.res = 0;
     }
 
     guess() {
-
+        this.res = Math.round((this.max + this.min) / 2);
+        return this.res;
     }
 
     lower() {
-
+        this.max = this.res
     }
 
     greater() {
-
+        this.min = this.res
     }
 }
 
